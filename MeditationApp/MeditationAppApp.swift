8907dcf9dccd2fd08856dc65289410ab4611d616
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-12 17:35:31 +0600

    first commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Sergey Shcheglov on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
