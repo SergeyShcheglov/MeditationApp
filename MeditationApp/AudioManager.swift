f37f23afac620b990df274093013e77fcf2695c8
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-13 16:33:05 +0600

    change singleton to combine publisher

--- MeditationApp/AudioManager.swift
+++ MeditationApp/AudioManager.swift
@@ -8,21 +8,27 @@
 import Foundation
 import AVKit
 
-final class AudioManager {
-    static let shared = AudioManager()
+final class AudioManager: ObservableObject {
+//    static let shared = AudioManager()
     
     var player: AVAudioPlayer?
     
-    func startPlayer(track: String) {
+    func startPlayer(track: String, isPreview: Bool = false) {
         guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
             print("Resource not found: \(track)")
             return
         }
         
         do {
-            player = try AVAudioPlayer(contentsOf: url)
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
             
-            player?.play()
+            player = try AVAudioPlayer(contentsOf: url)
+            if isPreview {
+                player?.prepareToPlay()
+            } else {
+                player?.play()
+            }
         } catch {
             print("Failed to initialize player", error)
         }
