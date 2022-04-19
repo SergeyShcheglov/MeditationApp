37bfbd1d4f26f3dd39b1f444210a9afc6f0e3a51
Sergey Shcheglov <97shcheglov@gmail.com>
2022-04-19 19:28:24 +0600

    add play/pause, loop and stop methods

--- MeditationApp/AudioManager.swift
+++ MeditationApp/AudioManager.swift
@@ -9,9 +9,16 @@ import Foundation
 import AVKit
 
 final class AudioManager: ObservableObject {
-//    static let shared = AudioManager()
+    //    static let shared = AudioManager()
     
     var player: AVAudioPlayer?
+    @Published private(set) var isPlaying: Bool = false {
+        didSet {
+            print("isPlaying", isPlaying)
+        }
+    }
+    
+    @Published private(set) var isLooping: Bool = false
     
     func startPlayer(track: String, isPreview: Bool = false) {
         guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
@@ -28,9 +35,45 @@ final class AudioManager: ObservableObject {
                 player?.prepareToPlay()
             } else {
                 player?.play()
+                isPlaying = true
             }
         } catch {
             print("Failed to initialize player", error)
         }
+    }
+    
+    func playPause() {
+        guard let player = player else {
+            print("instance of audio player is not found")
+            return
+        }
+        if player.isPlaying {
+            player.pause()
+            isPlaying = false
+        } else {
+            player.play()
+            isPlaying = true
+        }
+    }
+    
+    func stop() {
+        guard let player = player else {
+            return
+        }
+        
+        if player.isPlaying {
+            player.stop()
+            isPlaying = false
+        }
+    }
+    
+    func toggleLoop() {
+        guard let player = player else {
+            return
+        }
+        
+        player.numberOfLoops = player.numberOfLoops == 0 ? -1 : 0
+        isLooping = player.numberOfLoops != 0
+        print("isLooping", isLooping)
     }
 }
